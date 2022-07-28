d8e9ba87f83dee4cd227c45cfd894f542fae3e96
MiDev <razafintsalamamikaia@gmail.com>
2022-07-28 22:40:42 +0400

    restarting week 2 day 3 due to issue

--- Week 2/Day 3/script.js
+++
@@ -1,8 +0,0 @@
-// Exercise 1
-for (let i=0; i = 15; i++) {
-    if ((i % 2) == 0) {
-        console.log(i + " is an even number")
-    } else if ((i % 2) != 0) {
-        console.log(i + " is an odd number")
-    }
-}
