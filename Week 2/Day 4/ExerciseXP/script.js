056259627ec474930cbb078dcfd8ec3b9ad92d2a
MiDev <razafintsalamamikaia@gmail.com>
2022-08-01 12:18:24 +0400

    Week 2 Day 4 ExerciseXp

--- Week 2/Day 4/ExerciseXP/script.js
+++ Week 2/Day 4/ExerciseXP/script.js
@@ -1,26 +1,74 @@
-// Exercise 1
-function infoAboutMe() {
-    console.log("My name is Mikaia. I am currently 18 years old. My hobbies are music and programming.")
+// // Exercise 1
+// function infoAboutMe() {
+//     console.log("My name is Mikaia. I am currently 18 years old. My hobbies are music and programming.")
+// }
+
+// infoAboutMe()
+
+// // Exercise 2
+// function infoAboutPerson(personName, personAge, personFavoriteColor) {
+//     console.log("Your name is " + personName + ", you are " + personAge + " years old, your favorite color is " + personFavoriteColor )
+// }
+// infoAboutPerson("David", 45, "blue")
+// infoAboutPerson("Josh", 12, "yellow")
+
+// // Exercise 3
+// function calculateTip(){
+//     let bill = Number(prompt("How much is the bill: "))
+//     if (bill < 50 ) {
+//         tip = bill * 0.2
+//     } else if (bill > 50 && bill < 200) {
+//         tip = bill * 0.15
+//     } else if (bill > 200) {
+//         tip = bill * 0.10
+//     }
+//     console.log("Here is the tip you paid: $" +  tip + " Here is the final amount: $" + (tip+bill))
+// }
+
+// calculateTip()
+// // Exercise 3
+
+// function isDivisible(divisor) {
+//     divisor = Number(prompt("Type a divisor: "))
+//     let outcome = [];
+//     for (let i = 0; i < 501 ; i++){
+//         if ((i % divisor) == 0){
+//             outcome.push(i)
+//         } 
+//     }
+//     console.log("Outcome: " + outcome.join(" "))
+//     let sum = outcome.join("+")
+//     console.log ("Sum: " + eval(sum))
+// }
+
+// isDivisible()
+
+// Exercise 4
+
+let stock = {
+    "banana": 6,
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
 }
-
-infoAboutMe()
-
-// Exercise 2
-function infoAboutMe(personName, personAge, personFavoriteColor) {
-    console.log("Your name is " + personName + ", you are " + personAge + " years old, your favorite color is " + personFavoriteColor )
+let prices = {
+    "banana": 4,
+    "apple": 2,
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
 }
-infoAboutPerson("David", 45, "blue")
-infoAboutPerson("Josh", 12, "yellow")
-
-// Exercise 3
-function calculateTip(){
-    let bill = Number(prompt("How much is the bill: "))
-    if (bill < 50 ) {
-        tip = bill * 0.2
-    } else if (bill > 50 && bill < 200) {
-        tip = bill * 0.15
-    } else if (bill > 200) {
-        tip = bill * 0.10
+let shoppingList=["banana" , "orange" ,"apple"]
+console.log(stock[shoppingList[0]]) // To call an object details from an array
+function myBill(){
+    
+    let sum= 0;
+    for ( let x in shoppingList ){
+        if ( stock[shoppingList[x]] !==0){
+        sum=sum+ prices[shoppingList[x]]
+        }
     }
-    console.log("Here is the tip you paid: " +  tip + "Here is the final amount: " + (tip+bill))
+    return sum
 }
+console.log("My bill is " + myBill())
